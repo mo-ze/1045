31ab35e4cc7599d1f1b78999156e0b76fa87c13b
mo-ze <mo.ze@icloud.com>
2020-04-08 13:42:00 -0700

    first commit

---
+++ mt2/myscript.js
@@ -0,0 +1,64 @@
+let tr = document.getElementById("tr");
+let ci = document.getElementById("ci");
+let sq = document.getElementById("sq");
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext('2d');
+
+function square() {
+
+    ctx.fillStyle = sq.value;
+    ctx.fillRect(25, 25, 50, 50)
+    ctx.fill();
+}
+function circle() {
+
+    ctx.fillStyle = ci.value;
+    ctx.arc(50, 50, 50, 0, 2 * Math.PI)
+    ctx.fill()
+}
+function trangle() {
+
+    ctx.fillstyle = tr.value;
+    ctx.beginPath();
+    ctx.moveTo(0, 100);
+    ctx.lineTo(100, 100);
+    ctx.lineTo(50, 0)
+    ctx.lineTo(0, 100);
+    ctx.fill();
+
+}
+document.getElementById("display").onclick = function(){
+    let newInterval=setInterval(function(){
+    display();},500);
+}
+let i = 1;
+function display() {
+
+    if (i === 1) {
+        canvas.width = canvas.width
+        square();
+        console.log(i)
+        i++
+
+    }
+
+    else if (i === 2) {
+        canvas.width = canvas.width
+        trangle();
+        console.log(i)
+        i++
+
+
+    }
+
+    else if (i === 3) {
+        canvas.width = canvas.width
+        circle();
+        
+        console.log(i)
+        i = 1
+        
+    }
+
+}
+
